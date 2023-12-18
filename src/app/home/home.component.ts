e22d74408b912039538a094133ee89730c3d4303
muthulakshmil@gmail.com <muthulakshmil@gmail.com>
2023-12-18 09:53:32 +0530

    all certificate done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,11 +13,14 @@ export class HomeComponent implements OnInit {
   showHeader = false;
   viewHeader = false;
   username:string = "";
+  j = 0;
   // name = "Certificate Service";
   private jsonfile = "assets/data.json";
   private jsonfile1 ="assets/data1.json";
   jsonData: any;
   jsonData1: any;
+  certificateFields: string[]=[];
+  certificates: string[]=[];
   constructor(private http: HttpClient, private router:Router) { }
   
   ngOnInit() {
@@ -49,12 +52,33 @@ export class HomeComponent implements OnInit {
   getJsonData1(): Observable<any> {
     return this.http.get(this.jsonfile1);
   }
-  viewCertificate(){
+  viewCertificate(name: any){
 
-    // this.viewHeader = true;
+     this.viewHeader = true;
 
     // // change this code for view the particular certificate
-    // this.getJsonData1().subscribe(data => {
+    console.log("view certificate : ", name);
+    const url = "http://localhost:8080/certificates/"+name; 
+    this.getData(url).subscribe(
+      (data) => {
+        this.jsonData1 = data;
+        this.certificateFields = Object.keys(this.jsonData1.certificates[0]);
+        this.certificates = this.jsonData.certificates;
+        console.log('Data received:', this.jsonData);
+        
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
+
+    
+
+    // Extract certificates
+    
+
+
+    //  this.getJsonData1().subscribe(data => {
     //   this.jsonData1 = data;
     //   console.log("Come here json data 1");
     //   console.log(this.jsonData1);
@@ -73,10 +97,12 @@ export class HomeComponent implements OnInit {
   ListCertificate(){
    
     // Example of how to use the getData method:
+    this.showHeader = true;
     const url = "http://localhost:8080/certificates"; 
     this.getData(url).subscribe(
       (data) => {
-        console.log('Data received:', data);
+        this.jsonData = data;
+        console.log('Data received:', this.jsonData[0].certificates[0].alias);
         
       },
       (error) => {
@@ -85,7 +111,9 @@ export class HomeComponent implements OnInit {
     );
   }
 
-
+  isObject(value: any): boolean {
+    return typeof value === 'object' && value !== null;
+  }
 
 
 
